00262b06b0ae9124141a5b2b193e4444484bb261
Dmitrii Ushakov <daushakov89@mail.ru>
2022-02-20 20:21:52 +0800

    fix: some bag node 16 to 12

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 module.exports = function reverse (n) {
-    let str = String(n)
-    let result = ''
-    for (let i = str.length - 1; i > -1  ; i-- ) {
-        if (str[i] != '-') {result += str[i]}
-    }
 
-
-  return Number(result) 
+    return Number(String(Math.abs(n)).split('').reverse().join(''))
 }
